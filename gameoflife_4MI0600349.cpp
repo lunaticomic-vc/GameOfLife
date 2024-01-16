f59c6226007fd4f7a47ec141372fa9c6e1b2e4e6
Danichka <78638969+lunaticomic-vc@users.noreply.github.com>
2024-01-16 20:40:15 +0200

    Changed the entire structure
    Replaced 2d bool array to 2d vector array to match the description of the task more accurately and changed the functionality of all modules to match

--- gameoflife_4MI0600349.cpp
+++ gameoflife_4MI0600349.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
+#include<vector>
+#include<string>
+#include<fstream>
+
 using namespace std;
 
-const int HEIGHT = 24, WIDTH = 80;
+vector<vector<bool>> field;
 
-bool field[HEIGHT][WIDTH];
-
-void initializeClearField(bool field[][80], size_t height, size_t width)
+void initializeField(vector<vector<bool>> &field, size_t height=8, size_t width=16)
 {
-    for (size_t i = 0; i < height; i++) 
+    vector<bool> row;
+
+    for (size_t i = 0; i < height; i++)
     {
-        for (size_t j = 0; j < width; j++) 
+        for (size_t j = 0; j < width; j++)
+        {
+            row.push_back(0);
+        }
+        field.push_back(row);
+        row.clear();
+    }
+}
+
+void clearField(vector<vector<bool>> &field, size_t height, size_t width)
+{
+    for (size_t i = 0; i < height; i++)
+    {
+        for (size_t j = 0; j < width; j++)
         {
             field[i][j] = 0;
         }
     }
 }
 
-int countNeighbours(bool (*field)[80], size_t height, size_t width, int coordinateX, int coordinateY)
-{
-    int countN = 0;
-    for (int i = coordinateX - 1; i <= coordinateX + 1; i++)
-    {
-        if (i >= 0 && i < height)
-        {
-            for (int j = coordinateY - 1; j <= coordinateY + 1; j++)
-            {
-                if(j>=0 && j<width && field[i][j] == true)countN++;
-            }
-        }
-    }
-    return countN;
-}
-
-void printField(bool field[][80], size_t height, size_t width)
+void printField(vector<vector<bool>>& field, size_t height, size_t width)
 {
     cout << "Current field:" << endl;
     for (size_t i = 0; i < height; i++)
@@ -46,62 +47,204 @@ void printField(bool field[][80], size_t height, size_t width)
     }
 }
 
-
-void widenUp(bool(*field)[80], size_t height, size_t width, int shiftStep)
+int countNeighbours(vector<vector<bool>> field, size_t height, size_t width, size_t coordinateX, size_t coordinateY)
 {
-    shiftStep = shiftStep * (-1);
-    height += shiftStep+1;
-    for (size_t i = height-1; i > shiftStep; i--)
+    int countN = 0;
+
+    if (coordinateX > 0 && coordinateX < height - 1)
     {
-        for (size_t j = width-1; j>=0; j--)
+        if (coordinateY > 0 && coordinateY < width - 1)
         {
-            field[i][j] = field[i-1][j];
+            for (int i = coordinateX - 1; i <= coordinateX + 1; i++)
+            {
+                for (int j = coordinateY - 1; j <= coordinateY + 1; j++)
+                {
+                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
+                }
+            }
         }
+        else if (coordinateY == 0)
+        {
+            for (int i = coordinateX - 1; i <= coordinateX + 1; i++)
+            {
+                for (int j = coordinateY; j <= coordinateY + 1; j++)
+                {
+                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
+                }
+            }
+        }
+        else
+        {
+            for (int i = coordinateX - 1; i <= coordinateX + 1; i++)
+            {
+                for (int j = coordinateY - 1; j <= coordinateY; j++)
+                {
+                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
+                }
+            }
+        }
+    }
+    else if (coordinateX == 0)
+    {
+        if (coordinateY > 0 && coordinateY < width - 1)
+        {
+            for (int i = coordinateX ; i <= coordinateX + 1; i++)
+            {
+                for (int j = coordinateY - 1; j <= coordinateY + 1; j++)
+                {
+                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
+                }
+            }
+        }
+        else if (coordinateY == 0)
+        {
+            for (int i = coordinateX; i <= coordinateX + 1; i++)
+            {
+                for (int j = coordinateY; j <= coordinateY + 1; j++)
+                {
+                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
+                }
+            }
+
+        }
+        else
+        {
+            for (int i = coordinateX; i <= coordinateX + 1; i++)
+            {
+                for (int j = coordinateY - 1; j <= coordinateY; j++)
+                {
+                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
+                }
+            }
+
+        }
+    }
+    else
+    {
+        if (coordinateY > 0 && coordinateY < width - 1)
+        {
+            for (int i = coordinateX-1; i <= coordinateX; i++)
+            {
+                for (int j = coordinateY - 1; j <= coordinateY + 1; j++)
+                {
+                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
+                }
+            }
+        }
+        else if (coordinateY == 0)
+        {
+            for (int i = coordinateX-1; i <= coordinateX; i++)
+            {
+                for (int j = coordinateY; j <= coordinateY + 1; j++)
+                {
+                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
+                }
+            }
+
+        }
+        else
+        {
+            for (int i = coordinateX-1; i <= coordinateX; i++)
+            {
+                for (int j = coordinateY - 1; j <= coordinateY; j++)
+                {
+                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
+                }
+            }
+
+        }
+    }
+    return countN;
+}
+
+void keepNeighboursCount(int** fieldCountN, vector<vector<bool>>& field, size_t height, size_t width)
+{
+    for (size_t i = 0; i < height; i++)
+    {
+        fieldCountN[i] = new int[width];
+        for (size_t j = 0; j < width; j++)
+        {
+            fieldCountN[i][j] = countNeighbours(field, height, width, i, j);
+            cout << fieldCountN[i][j];
+        }
+        cout << endl;
     }
 }
 
-void widenLeft(bool(*field)[80], size_t &height, size_t &width, int shiftStep)
+void stepForward(int** countNeighbours, vector<vector<bool>>& field, size_t height, size_t width)
 {
-    shiftStep = shiftStep * (-1);
-    width += shiftStep + 1;
-    for (size_t i = height-1; i >= 0; i--)
-    {
-        for (size_t j = width-1; j > shiftStep; j--)
-        {
-            field[i][j] = field[i][j-1];
-        }
-    }
+    keepNeighboursCount(countNeighbours, field, height, width);
 
-}
-
-void stepForward(bool(*field)[80], size_t height, size_t width)
-{
     for (size_t i = 0; i < height; i++)
     {
         for (size_t j = 0; j < width; j++)
         {
-            if (field[i][j]==1 && (countNeighbours(field, height, width, i, j) < 2 || countNeighbours(field, height, width, i, j) > 3))
+            if (field[i][j] == 1 && (countNeighbours[i][j] < 2 || countNeighbours[i][j] > 3))
             {
                 field[i][j] = 0;
             }
-            else if (field[i][j] == 0 && countNeighbours(field, height, width, i, j) == 3)
+            else if (field[i][j] == 0 && countNeighbours[i][j] == 3)
             {
                 field[i][j] = 1;
             }
-            
+
         }
     }
 }
 
-void resizeField(bool(*field)[80], size_t &oldHeight, size_t &oldWidth, size_t newHeight, size_t newWidth)
+void widenUpOrDown(vector<vector<bool>> &field, size_t &height, size_t &width, int shiftStep, bool isDirectionUp)
+{
+    vector<bool> row;
+
+    int insertStep = 0;
+
+    for (int i = 0; i < width; i++)
+    {
+        row.push_back(0);
+    }
+    if (isDirectionUp == 0) insertStep += field.size();
+
+    for (int i = 0; i < shiftStep; i++)
+    {
+        field.insert(field.begin() + insertStep, row);
+    }
+    
+    height += shiftStep;
+}
+
+void widenLeftOrRight(vector<vector<bool>> &field, size_t &height, size_t &width, int shiftStep, bool isDirectionLeft)
+{
+    vector<bool> col;
+
+    int insertStep = 0;
+
+    if (isDirectionLeft == 0) insertStep += field[0].size();
+
+    for (int i = 0; i < height; i++)
+    {
+        field[i].insert(field[i].begin() + insertStep, 0);
+    }
+
+    width += shiftStep;
+}
+
+void resizeField(vector<vector<bool>> &field, size_t &oldHeight, size_t &oldWidth, size_t newHeight, size_t newWidth)
 {
     if (oldHeight > newHeight)
     {
         for (size_t i = newHeight; i < oldHeight; i++)
         {
-            for (size_t j = 0; j < newWidth; j++)
+            field[i].clear();
+        }
+    }
+    else
+    {
+        for (size_t i = oldHeight; i < newHeight; i++)
+        {
+            field.push_back({0});
+            for (size_t j = 0; j < oldWidth; j++)
             {
-                field[i][j] = 0;
+                field[i].push_back(0);
             }
         }
     }
@@ -112,63 +255,126 @@ void resizeField(bool(*field)[80], size_t &oldHeight, size_t &oldWidth, size_t n
         {
             for (size_t j = newWidth; j < oldWidth; j++)
             {
-                field[i][j] = 0;
+                field[i].pop_back();
+            }
+        }
+    }
+    else
+    {
+        for (size_t i = 0; i < oldHeight; i++)
+        {
+            for (size_t j = oldWidth; j < newWidth; j++)
+            {
+                field[i].push_back(0);
             }
         }
     }
     oldWidth = newWidth;
 }
 
-void toggleCell(bool field[][80], size_t &height, size_t &width, int coordinateX, int coordinateY)
+void toggleCell(vector<vector<bool>> &field, size_t &height, size_t &width, int coordinateX, int coordinateY)
 {
-    if (coordinateX <= 0)widenUp(field, height, width, coordinateX);
-    else if (coordinateX >= height) height = coordinateX;
+    coordinateX--;
+    coordinateY--;
 
-    if (coordinateY <= 0)widenLeft(field, height, width, coordinateY);
-    else if (coordinateY >= width) width = coordinateY;
+    bool doWidenUp, doWidenLeft;
 
-    if (field[coordinateX][coordinateY] == true) field[coordinateX][coordinateY] = false;
-    else field[coordinateX][coordinateY] = true;
+    if (coordinateX < 0) doWidenUp = 1;
+    else if (coordinateX >= height) doWidenUp = 0;
 
-    cout << field[coordinateX][coordinateY] << endl;
+    if(coordinateX< 0 || coordinateX >= height) widenUpOrDown(field, height, width, coordinateX, doWidenUp);
+
+    if (coordinateY < 0) doWidenLeft = 1;
+    else if (coordinateY >= width) doWidenLeft = 0;
+
+    if (coordinateY<0 || coordinateX >= height) widenLeftOrRight(field, height, width, coordinateX, doWidenLeft);
+
+    field[coordinateX][coordinateY] = !(field[coordinateX][coordinateY]);
 }
 
-void saveToFile(bool** field)
-{
-
-}
-
-void loadFile(bool** field)
-{
-
-}
-
-void readMatrix(bool(*field[80]))
+void saveToFile(vector<vector<bool>> field)
 {
 
 }
 
-void readMenuCommand(int n)
+void randomizeField()
 {
+
+}
+
+bool translateFieldCharToBool(string row, int index)
+{
+    if (row[index] == '@')return 1;
+    else return 0;
+}
+
+void readFile()
+{
+    std::ifstream file("C:\C++\GameOfLife\x64\Debug\gay.txt");
+
+    if (!file.is_open()) {
+        std::cerr << "Unable to open file\n";
+
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+
+    file.close();
+}
+
+void loadFile(vector<vector<bool>>& field, string nameOfFile, size_t& height, size_t& width)
+{
+    ifstream inputFile(nameOfFile);
+
+    string row;
+
+    int indRow = 0, indCol = 0;
+
+
+    if (inputFile.is_open())
+    {
+        while (getline(inputFile, row))
+        {
+            field.push_back({});
+            for (int i = 0; i < row.length(); i++)
+            {
+                cout << translateFieldCharToBool(row, i);
+                field[indRow].push_back(translateFieldCharToBool(row, i));
+            }
+            cout << endl;
+            indRow++;
+        }
+        inputFile.close();
+    }
+    cout << indRow << endl;
+    height = indRow;
+    width = field[0].size();
 
 }
 
 int main()
 {
     int begin, step = 0;
-    size_t h, w, newH, newW, coordinateX, coordinateY;
+
+    size_t h = 8, w = 16;
+    size_t newH, newW, coordinateX, coordinateY;
+
+    string fileName, row;
+
+    int** neighboursCount = new int* [h];
     cout << "Welcome to the Game of Life! Please choose an option:" << endl;
     cout << "For \"New game\" please enter 1...\nFor \"Load game\" please enter 2...\nFor \"Exit\" please enter 3..." << endl;
     cin >> begin;
-    if (begin == 1)
-    {
-        h = 8;
-        w = 16;
-        initializeClearField(field, h, w);
-    }
+    if (begin == 1)initializeField(field);
     else if (begin == 2)
     {
-
+        /*cout << "Enter filename: " << endl;
+        cin >> fileName;*/
+        //loadFile(field, "C:\C++\GameOfLife\\x64\Debug\gay.txt", h, w);
+        readFile();
     }
     else if (begin==3) return 0;
 
@@ -185,12 +391,12 @@ int main()
 
         switch (step)
         {
-        case 4: stepForward(field, h, w); break;
+        case 4: stepForward(neighboursCount, field, h, w); break;
         case 5: 
         {
             cout << "PLease input new height: ";
             cin >> newH;
-            cout << "PLease input new width";
+            cout << "PLease input new width: ";
             cin >> newW;
             resizeField(field, h, w, newH, newW);
         }; break;
@@ -206,11 +412,18 @@ int main()
         }; break;
         case 7:
         {
-            initializeClearField(field, h, w);
+            clearField(field, h, w);
         } break;
         case 8:; break;
         case 9:; break;
         }
     }
+
+    for (int i = 0; i < h; ++i) 
+    {
+        delete[] neighboursCount[h];
+    }
+
+    delete neighboursCount;
 
 }
