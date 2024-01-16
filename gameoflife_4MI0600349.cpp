9ee9b6b0d401f20fb15a95b00495e307ad95a2ba
Danichka <78638969+lunaticomic-vc@users.noreply.github.com>
2024-01-17 00:17:52 +0200

    Deleted debugging checks, added functionality

--- gameoflife_4MI0600349.cpp
+++ gameoflife_4MI0600349.cpp
@@ -12,14 +12,14 @@ void initializeField(vector<vector<bool>> &field, size_t height=8, size_t width=
 {
     vector<bool> row;
 
+    for (size_t j = 0; j < width; j++)
+    {
+        row.push_back(0);
+    }
+
     for (size_t i = 0; i < height; i++)
     {
-        for (size_t j = 0; j < width; j++)
-        {
-            row.push_back(0);
-        }
         field.push_back(row);
-        row.clear();
     }
 }
 
@@ -52,113 +52,37 @@ int countNeighbours(vector<vector<bool>> field, size_t height, size_t width, siz
 {
     int countN = 0;
 
+    int beginLoopHeight = coordinateX, endLoopHeight = coordinateX;
+    int beginLoopWidth = coordinateY, endLoopWidth = coordinateY;
+
     if (coordinateX > 0 && coordinateX < height - 1)
     {
-        if (coordinateY > 0 && coordinateY < width - 1)
-        {
-            for (int i = coordinateX - 1; i <= coordinateX + 1; i++)
-            {
-                for (int j = coordinateY - 1; j <= coordinateY + 1; j++)
-                {
-                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
-                }
-            }
-        }
-        else if (coordinateY == 0)
-        {
-            for (int i = coordinateX - 1; i <= coordinateX + 1; i++)
-            {
-                for (int j = coordinateY; j <= coordinateY + 1; j++)
-                {
-                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
-                }
-            }
-        }
-        else
-        {
-            for (int i = coordinateX - 1; i <= coordinateX + 1; i++)
-            {
-                for (int j = coordinateY - 1; j <= coordinateY; j++)
-                {
-                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
-                }
-            }
-        }
+        beginLoopHeight--;
+        endLoopHeight++;
     }
-    else if (coordinateX == 0)
+    else if (coordinateX == 0) endLoopHeight++;
+    else beginLoopHeight--;
+
+    if (coordinateY > 0 && coordinateY < width - 1)
     {
-        if (coordinateY > 0 && coordinateY < width - 1)
-        {
-            for (int i = coordinateX ; i <= coordinateX + 1; i++)
-            {
-                for (int j = coordinateY - 1; j <= coordinateY + 1; j++)
-                {
-                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
-                }
-            }
-        }
-        else if (coordinateY == 0)
-        {
-            for (int i = coordinateX; i <= coordinateX + 1; i++)
-            {
-                for (int j = coordinateY; j <= coordinateY + 1; j++)
-                {
-                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
-                }
-            }
-
-        }
-        else
-        {
-            for (int i = coordinateX; i <= coordinateX + 1; i++)
-            {
-                for (int j = coordinateY - 1; j <= coordinateY; j++)
-                {
-                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
-                }
-            }
-
-        }
+        beginLoopWidth--;
+        endLoopWidth++;
     }
-    else
+    else if (coordinateY == 0) endLoopWidth++;
+    else beginLoopWidth--;
+
+    for (int i = beginLoopHeight; i <= endLoopHeight; i++)
     {
-        if (coordinateY > 0 && coordinateY < width - 1)
+        for (int j = beginLoopWidth; j <= endLoopWidth; j++)
         {
-            for (int i = coordinateX-1; i <= coordinateX; i++)
-            {
-                for (int j = coordinateY - 1; j <= coordinateY + 1; j++)
-                {
-                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
-                }
-            }
-        }
-        else if (coordinateY == 0)
-        {
-            for (int i = coordinateX-1; i <= coordinateX; i++)
-            {
-                for (int j = coordinateY; j <= coordinateY + 1; j++)
-                {
-                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
-                }
-            }
-
-        }
-        else
-        {
-            for (int i = coordinateX-1; i <= coordinateX; i++)
-            {
-                for (int j = coordinateY - 1; j <= coordinateY; j++)
-                {
-                    if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
-                }
-            }
-
+            if (field[i][j] == true && !(i == coordinateX && j == coordinateY))countN++;
         }
     }
+
     return countN;
 }
 
-void keepNeighboursCount(int** fieldCountN, vector<vector<bool>>& field, size_t height, size_t width)
+void keepNeighboursCount(int** fieldCountN, vector<vector<bool>> &field, size_t height, size_t width)
 {
     for (size_t i = 0; i < height; i++)
     {
@@ -323,6 +247,12 @@ bool translateFieldCharToBool(string row, int index)
     else return 0;
 }
 
+char translateFieldBoolToChar(vector<bool> row, int index)
+{
+    if (row[index] == 1)return '@';
+    else return '-';
+}
+
 void loadFile(vector<vector<bool>>& field, string nameOfFile, size_t& height, size_t& width)
 {
     ifstream inputFile(nameOfFile);
@@ -346,80 +276,148 @@ void loadFile(vector<vector<bool>>& field, string nameOfFile, size_t& height, si
     width = field[0].size();
 }
 
-void saveToFile(vector<vector<bool>> field)
+void saveToFile(vector<vector<bool>> field, string fileName, size_t height, size_t width)
 {
+    ofstream outFile(fileName);
 
+    if (outFile.is_open()) 
+    {
+        for (size_t i = 0; i<height; i++) 
+        {
+            for (size_t j = 0; j < width; j++)
+            {
+                outFile << translateFieldBoolToChar(field[i], j);
+            }
+            outFile << endl;
+        }
+        outFile.close(); 
+    }
+    else 
+    {
+        cout << "Unable to open file for writing." << endl;
+    }
+}
+
+void errorMessage()
+{
+    cout << "Invalid output. Please provide output according to menu rules." << endl;
+}
+
+void endMessage()
+{
+    cout << "Thank you for playing. Please come again!" << endl;
 }
 
 int main()
 {
     int begin, step = 0, n;
-
     size_t h = 8, w = 16;
     size_t newH, newW, coordinateX, coordinateY;
-
     string fileName, row;
-
     int** neighboursCount = new int* [h];
+
+    beginningOfGame:
+
     cout << "Welcome to the Game of Life! Please choose an option:" << endl;
     cout << "For \"New game\" please enter 1...\nFor \"Load game\" please enter 2...\nFor \"Exit\" please enter 3..." << endl;
     cin >> begin;
+
     if (begin == 1)initializeField(field);
+
     else if (begin == 2)
     {
         cout << "Enter filename:\nNote: For maximum accuracy, please provide absolute filepath.\nE.g.: C\\C++\\GameOfLife\\example.txt NOT example.txt!" << endl;
         cin >> fileName;
         loadFile(field, fileName, h, w);
     }
-    else if (begin == 3) return 0;
+
+    else if (begin == 3)
+    {
+        endMessage();
+        return 0;
+    }
+
+    else
+    {
+        errorMessage();
+        goto beginningOfGame;
+    }
 
     while (step != 10)
     {
-        cout << "tuka li bugvash dac ti eba maikara" << endl;
         printField(field, h, w);
-        cout << "tuka li bugvash dac ti eba maikara ETOOOOOOOOOOO" << endl;
+
+        makeMove:
 
         cout << "Please choose an option:" << endl;
-        cout << "For \"Step forward\" please enter 4...\nFor \"Resize\" please enter 5...\nFor \"Toggle cell\" please enter 6..." << endl;
-        cout << "For \"Clear\" please enter 7...\nFor \"Randomize\" please enter 8...\nFor \"Save to file\" please enter 9..." << endl;
+        cout << "For \"Step forward\" please enter 4..." << endl;
+        cout << "For \"Resize\" please enter 5..." << endl;
+        cout << "For \"Toggle cell\" please enter 6..." << endl;
+        cout << "For \"Clear\" please enter 7..." << endl; 
+        cout << "For \"Randomize\" please enter 8..." << endl; 
+        cout << "For \"Save to file\" please enter 9..." << endl;
         cout << "For \"End\" please enter 10" << endl;
 
         cin >> step;
 
+        if (cin.fail())
+        {
+            errorMessage();
+            goto makeMove;
+        }
+
         switch (step)
         {
-        case 4: stepForward(neighboursCount, field, h, w); break;
-        case 5:
-        {
-            cout << "PLease input new height: ";
-            cin >> newH;
-            cout << "PLease input new width: ";
-            cin >> newW;
-            resizeField(field, h, w, newH, newW);
-        }; break;
-        case 6:
-        {
-            cout << "PLease input coordinate X: ";
-            cin >> coordinateX;
-            cout << "PLease input coordinate Y: ";
-            cin >> coordinateY;
-            toggleCell(field, h, w, coordinateX, coordinateY);
-            cout << "Check 5" << endl;
-        }; break;
-        case 7:
-        {
-            clearField(field, h, w);
-        } break;
-        case 8: 
-        {
-            cout << "Please enter randomization quotient: " << endl;
-            cin >> n;
-            randomizeField(field, h, w, n);
-        };
-        break;
-        case 9:; break;
+            case 4: 
+            {
+                stepForward(neighboursCount, field, h, w);
+                break;
+            }
+            case 5:
+            {
+                cout << "PLease input new height: ";
+                cin >> newH;
+                cout << "PLease input new width: ";
+                cin >> newW;
+                resizeField(field, h, w, newH, newW);
+                break;
+            }
+            case 6:
+            {
+                cout << "PLease input coordinate X: ";
+                cin >> coordinateX;
+                cout << "PLease input coordinate Y: ";
+                cin >> coordinateY;
+                toggleCell(field, h, w, coordinateX, coordinateY);
+                break;
+            }
+            case 7:
+            {
+                clearField(field, h, w);
+                break;
+            }
+            case 8: 
+            {
+                cout << "Please enter randomization quotient: " << endl;
+                cin >> n;
+                randomizeField(field, h, w, n);
+                break;
+            }
+            case 9:
+            {
+                cout << "Please provide a filename for saving your results." << endl;
+                cin >> fileName;
+                saveToFile(field, fileName, h, w);
+                break;
+            }
+            default: 
+            {
+                errorMessage();
+            }
         }
     }
+
+    endMessage();
 
     for (int i = 0; i < h; ++i)
     {
@@ -427,5 +425,7 @@ int main()
     }
 
     delete[] neighboursCount;
+
+    return 0;
 
 }
